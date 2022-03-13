eda11881315491ef158ec4e2772fc1fd96fec397
Diego Gallovich <dieguiviti@gmail.com>
2022-03-13 19:51:26 +0100

    posts service first commit

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,1 +1,29 @@
-console.log('hello world');
+import express from 'express';
+import { randomBytes } from 'crypto';
+
+const server = express();
+
+server.use(express.json());
+
+const posts: Record<string, { title: string; id: string }> = {};
+
+// Get posts
+server.get('/posts', (_req, res) => {
+   res.send(posts);
+});
+
+// Post post
+server.post('/posts', (req, res) => {
+   const id = randomBytes(4).toString('hex');
+
+   const { title } = req.body;
+
+   posts[id] = {
+      id,
+      title,
+   };
+
+   res.status(201).send(posts[id]);
+});
+
+server.listen(4000, () => console.log('listening on port:', 4000));
