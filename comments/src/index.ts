d640fb9b8c0aa7291a8c813dd35ba5ac83eee155
Diego Gallovich <dieguiviti@gmail.com>
2022-03-13 19:51:13 +0100

    comments service first commit

--- comments/src/index.ts
+++ comments/src/index.ts
@@ -1,1 +1,35 @@
-console.log('hello world');
+import express from 'express';
+import { randomBytes } from 'crypto';
+
+const server = express();
+
+server.use(express.json());
+
+// Comment type
+type Comments = Array<{ content: string; id: string }>;
+
+const commentsByPostId: Record<string, Comments> = {};
+
+// Get comments
+server.get('/posts/:id/comments', (req, res) => {
+   res.send(commentsByPostId[req.params.id]);
+});
+
+// Post a comment
+server.post('/posts/:id/comments', (req, res) => {
+   const { content } = req.body;
+
+   const postId = req.params.id;
+
+   const comments = commentsByPostId[postId] || [];
+
+   const newCommentId = randomBytes(4).toString('hex');
+
+   const newComment = { id: newCommentId, content };
+
+   comments.unshift(newComment);
+
+   res.status(201).send(comments);
+});
+
+server.listen(4001, () => console.log('listening on port:', 4001));
