79b34afacb0c527e8941f364847615b2008b39e8
Diego Gallovich <dieguiviti@gmail.com>
2022-03-26 14:23:26 +0100

    FormContainer, Form, FormGroup, Label, Input and PrimaryButton Styled component instances created

---
+++ client/src/components/styled.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const FormContainer = styled.div``;
+
+export const Form = styled.form``;
+
+export const FormGroup = styled.div``;
+
+export const Label = styled.label``;
+
+export const Input = styled.input``;
+
+export const PrimaryButton = styled.button``;
