eda97a01e7605f17ff5fccce9991123b082dd5a4
Diego Gallovich <dieguiviti@gmail.com>
2022-03-26 14:24:00 +0100

    PostsCreationForm Component markup structure

---
+++ client/src/components/Posts/PostsCreationForm.tsx
@@ -0,0 +1,16 @@
+import { FormContainer, Form, FormGroup, Label, Input, PrimaryButton } from '../styled';
+
+export const PostsCreationForm = () => {
+   return (
+      <FormContainer>
+         <Form name="createPost">
+            <FormGroup>
+               <Label htmlFor="title">Title</Label>
+               <Input id="title" type="text" placeholder="A wonderful post" />
+            </FormGroup>
+
+            <PrimaryButton type="submit">Submit</PrimaryButton>
+         </Form>
+      </FormContainer>
+   );
+};
