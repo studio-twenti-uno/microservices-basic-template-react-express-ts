ef0f0884a5d6bf696c1036df34c082ad3bf0714d
Diego Gallovich <dieguiviti@gmail.com>
2022-04-09 15:15:12 +0200

    Handling submit event

--- client/src/components/Posts/PostsCreationForm.tsx
+++ client/src/components/Posts/PostsCreationForm.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+import axios from 'axios';
 import {
    FormContainer,
    Form,
@@ -8,15 +10,48 @@ import {
 } from '../styled';
 
 export const PostsCreationForm = () => {
+   // State
+   const [title, setTitle] = useState('');
+
+   // handlers
+   const handleChange = (
+      e: ChangeEvent<HTMLInputElement>,
+   ) => {
+      setTitle(e.target.value);
+   };
+
+   const handleSubmit = async (e: FormEvent) => {
+      e.preventDefault();
+
+      try {
+         const response = await axios({
+            method: 'post',
+            url: 'http://localhost:4000/posts',
+            data: {
+               title,
+            },
+            withCredentials: true,
+         });
+
+         console.log(response);
+         if (response.status === 201) {
+            setTitle('');
+         }
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
    return (
       <FormContainer>
-         <Form name="createPost">
+         <Form name="createPost" onSubmit={handleSubmit}>
             <FormGroup>
                <Label htmlFor="title">Title</Label>
                <Input
                   id="title"
                   type="text"
                   placeholder="A wonderful post"
+                  onChange={handleChange}
                />
             </FormGroup>
 
