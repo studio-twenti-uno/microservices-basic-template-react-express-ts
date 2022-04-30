ad7822ab6af39165be88491836ac1ade7cb2398b
Diego Gallovich <dieguiviti@gmail.com>
2022-04-30 20:44:55 +0200

    Emitting Post Creation events and Receiving events

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { randomBytes } from 'crypto';
+import axios from 'axios';
 
 const server = express();
 
@@ -15,7 +16,7 @@ server.get('/posts', (_req, res) => {
 });
 
 // Post post
-server.post('/posts', (req, res) => {
+server.post('/posts', async (req, res) => {
    const id = randomBytes(4).toString('hex');
 
    const { title } = req.body;
@@ -25,7 +26,31 @@ server.post('/posts', (req, res) => {
       title,
    };
 
+   // Emit event
+   try {
+      const response = await axios({
+         method: 'post',
+         url: 'http://localhost:4005/events',
+         data: {
+            type: 'PostCreated',
+            payload: {
+               id,
+               title,
+            },
+         },
+      });
+   } catch (error) {
+      console.log('Error posting event to event bus\n', error);
+   }
+
    res.status(201).send(posts[id]);
+});
+
+// Event receiver
+server.post('/events', (req, res) => {
+   console.log('received event: ', req.body.type);
+
+   res.send({});
 });
 
 server.listen(4000, () => console.log('listening on port:', 4000));
