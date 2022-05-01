971ebcaae1fe0061cfce8dadac5a95ecd44c7fd3
Diego Gallovich <dieguiviti@gmail.com>
2022-05-01 14:56:46 +0200

    Moderation service implementation and event emission

--- moderation/src/index.ts
+++ moderation/src/index.ts
@@ -22,13 +22,38 @@ type Event = CommentCreationEvent;
 server.post('/events', async (req, res) => {
    const event: Event = req.body;
 
+   console.log('received event: ', req.body.type);
+
    switch (event.type) {
       case 'CommentCreated': {
+         const status = event.payload.content.toLowerCase().includes('orange')
+            ? 'rejected'
+            : 'approved';
+
+         try {
+            await axios({
+               method: 'post',
+               url: 'http://localhost:4005/events',
+               data: {
+                  type: 'CommentModerated',
+                  payload: {
+                     ...event.payload,
+                     status,
+                  },
+               },
+            });
+         } catch (error) {
+            console.log(
+               'error while sending CommentModerated event to event bus\n',
+               error,
+            );
+         }
+
          break;
       }
 
       default: {
-         console.log('received unregistered event', req.body);
+         console.log('ignored event', req.body);
 
          break;
       }
